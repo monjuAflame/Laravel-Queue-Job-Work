7a38af698c3d0589d3f177211176e076a2e82edc
monjuAflame <monjuaflame@gmail.com>
2023-01-08 13:07:47 +0600

    send email by queue with driver database

--- app/Notifications/RegisteredUserNotification.php
+++ app/Notifications/RegisteredUserNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RegisteredUserNotification extends Notification
+class RegisteredUserNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
